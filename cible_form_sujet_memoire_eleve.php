b263ac2868fe9c63220b66323f3ae0d1f894f642
hls1138 <hlsergent@gmail.com>
2016-10-21 15:55:38 +0200

    Update cible_form_sujet_memoire_eleve.php

--- cible_form_sujet_memoire_eleve.php
+++ cible_form_sujet_memoire_eleve.php
@@ -6,15 +6,23 @@
     </head>
     <body>
         <h1>Récapitulatif de proposition de sujet de mémoire</h1>
+        
+        
+        
+        
+        <input type="radio" name="valide" value="oui" id="oui" checked="checked" /> <label for="oui">Oui</label>
+        <input type="radio" name="a reformuler" value="non" id="non" /> <label for="non">Non</label>
+        <input type="radio" name="refusé" value="non" id="non" /> <label for="non">Non</label>
+  
         <?php
         // put your code here
-        // identification starts here
+        // identification starts here 
         $_POST['eleve_prenom'];
         $_POST['eleve_nom'];
         $_POST['eleve_memoire_sujet_num_version'];
         $_POST['eleve_session'];
 
-        // supervisor intervention starts here
+        // intervention starts here
         $_POST['eleve_memoire_sujet_titre'];
         $_POST['encadrant_proposition'];
         $_POST['eleve_problematique'];
