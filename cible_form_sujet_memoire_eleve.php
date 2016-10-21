bbce00f39f40af800b5479d3cc7f3c01398727a3
dadysdesign <app@dadys.info>
2016-10-21 15:38:35 +0200

    ajout_cible_memoire_a_faire

---
+++ cible_form_sujet_memoire_eleve.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Reception sujet mémoire</title>
+    </head>
+    <body>
+        <h1>Récapitulatif de proposition de sujet de mémoire</h1>
+        <?php
+        // put your code here
+        $_POST['eleve_prenom'];
+        $_POST['eleve_nom'];
+//        $_POST['eleve_prenom'];
+//        $_POST['eleve_prenom'];
+
+        ?>
+    </body>
+</html>
