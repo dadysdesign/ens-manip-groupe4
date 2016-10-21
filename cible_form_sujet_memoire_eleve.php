0b769a6060a83623a1a3791ed350e9324b0d0017
keikhcheung <kei.khcheung@gmail.com>
2016-10-21 15:57:26 +0200

    Update cible_form_sujet_memoire_eleve.php

--- cible_form_sujet_memoire_eleve.php
+++ cible_form_sujet_memoire_eleve.php
@@ -9,10 +9,10 @@
         
         
         
-        
+        <!-- Radio buttons -->
         <input type="radio" name="valide" value="oui" id="oui" checked="checked" /> <label for="oui">Validé</label>
         <input type="radio" name="refusé" value="non" id="non" /> <label for="non">Refusé</label>
-        <input type="radio" name="a reformuler" value="non" id="non" /> <label for="non">À reformuler</label>
+        <input type="radio" name="a reformuler" value="non" id="non" /> <label for="reform">À reformuler</label>
   
         <?php
         // put your code here
