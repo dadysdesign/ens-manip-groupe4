bc186819e75cde42b4d43fe3af6e7c935ca5cc4d
keikhcheung <kei.khcheung@gmail.com>
2016-10-21 15:44:36 +0200

    recap_all_var

--- cible_form_sujet_memoire_eleve.php
+++ cible_form_sujet_memoire_eleve.php
@@ -10,6 +10,16 @@
         // put your code here
         $_POST['eleve_prenom'];
         $_POST['eleve_nom'];
+        $_POST['eleve_memoire_sujet_num_version'];
+        $_POST['eleve_session'];
+        $_POST['eleve_memoire_sujet_titre'];
+        $_POST['encadrant_proposition'];
+        $_POST['eleve_problematique'];
+        $_POST['eleve_deroulement'];
+        $_POST['eleve_contribution_ai'];
+        $_POST['Valider'];
+
+
 //        $_POST['eleve_prenom'];
 //        $_POST['eleve_prenom'];
 
