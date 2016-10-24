52289140a5cac7aaeed694b9dce6c0d0722249e1
keikhcheung <kei.khcheung@gmail.com>
2016-10-24 16:34:47 +0200

    add deroulement and problematique

--- proposition_sujet.sql
+++ proposition_sujet.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Oct 24, 2016 at 03:05 PM
+-- Generation Time: Oct 24, 2016 at 04:33 PM
 -- Server version: 5.5.42
 -- PHP Version: 7.0.8
 
@@ -27,11 +27,20 @@ CREATE TABLE `proposition_sujet` (
   `encadrant` varchar(256) NOT NULL,
   `num_version` int(11) NOT NULL,
   `apport_ai` text NOT NULL,
-  `date_depot` date NOT NULL,
+  `date_depot` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `session` varchar(256) NOT NULL,
   `statut_validation` varchar(256) NOT NULL,
-  `id_memoire_final` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id_memoire_final` int(11) NOT NULL,
+  `deroulement` text NOT NULL,
+  `problematique` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `proposition_sujet`
+--
+
+INSERT INTO `proposition_sujet` (`id`, `nom`, `sujet`, `encadrant`, `num_version`, `apport_ai`, `date_depot`, `session`, `statut_validation`, `id_memoire_final`, `deroulement`, `problematique`) VALUES
+(1, '', '', '', 0, '', '0000-00-00 00:00:00', '', '', 0, '', '');
 
 --
 -- Indexes for dumped tables
@@ -51,4 +60,4 @@ ALTER TABLE `proposition_sujet`
 -- AUTO_INCREMENT for table `proposition_sujet`
 --
 ALTER TABLE `proposition_sujet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
