5fb1c80ad4eae67d42eb90b3cb316f16f9556791
keikhcheung <kei.khcheung@gmail.com>
2016-10-25 12:38:19 +0200

    varchar pseudo/mdp

--- enseignant.sql
+++ enseignant.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Oct 25, 2016 at 11:45 AM
+-- Generation Time: Oct 25, 2016 at 12:37 PM
 -- Server version: 5.5.42
 -- PHP Version: 7.0.8
 
@@ -24,8 +24,8 @@ CREATE TABLE `enseignant` (
   `id` int(11) NOT NULL,
   `enseignant_nom` varchar(256) NOT NULL,
   `enseignant_prenom` varchar(256) NOT NULL,
-  `enseignant_pseudo` text NOT NULL,
-  `enseignant_mdp` text NOT NULL
+  `enseignant_pseudo` varchar(256) NOT NULL,
+  `enseignant_mdp` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
